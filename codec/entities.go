5484bfba700bf0d6ee3c7ced6ed7a92a5c6282e2
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-03-05 10:10:37 -0800

    common XML entities as []byte: &amp; &gt; &lt; &quot;

---
+++ codec/entities.go
@@ -0,0 +1,9 @@
+package xmlcodec
+
+// Common XML entities as []byte.
+var (
+	amp  []byte = []byte("&amp;")
+	gt   []byte = []byte("&gt;")
+	lt   []byte = []byte("&lt;")
+	quot []byte = []byte("&quot;")
+)
